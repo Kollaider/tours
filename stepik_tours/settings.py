6727f63e45254791fdb4499b48a624a8afa5ecaf
kolaider <kolja87@tut.by>
2021-05-14 00:05:20 +0300

    Added views

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rtrszj)h6&^wcl(%ani!y5l_sqskmw^#@bizagq*=(-6g-ssms
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainView(View):
+
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class DepartureView(View):
+
+    def get(self, request, departure):
+        return render(request, 'departure.html', context={'departure': departure})
+
+
+class TourView(View):
+
+    def get(self, request, id):
+        return render(request, 'tour.html', context={'id': id})
